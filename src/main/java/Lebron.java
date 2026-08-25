24b2db5d7a4e2bafafc91059423eb416e0fffb26
JimmyIsAwesome12 <jimmyisawesome4@gmail.com>
2026-08-25 16:57:04 +0800

    Level-2

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -15,11 +15,22 @@ public class Lebron {
         System.out.println("What can I do for you?");
         System.out.println(LINE);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         Scanner scanner = new Scanner(System.in);
         String command = scanner.nextLine();
         while (!command.equals("bye")) {
             System.out.println(LINE);
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("added: " + command);
+            }
             System.out.println(LINE);
             command = scanner.nextLine();
         }
