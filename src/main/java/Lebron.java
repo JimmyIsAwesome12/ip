4d973c3ba873aae784b31a9be11ea4e7614ab271
JimmyIsAwesome12 <jimmyisawesome4@gmail.com>
2026-08-25 16:54:08 +0800

    Level-1

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Lebron {
     private static final String LINE = "____________________________________________________________";
     private static final String BANNER = " _     _____ ____  ____   ___  _   _ \n"
@@ -13,6 +15,17 @@ public class Lebron {
         System.out.println("What can I do for you?");
         System.out.println(LINE);
 
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println(LINE);
+            System.out.println(command);
+            System.out.println(LINE);
+            command = scanner.nextLine();
+        }
+        scanner.close();
+
+        System.out.println(LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE);
     }
