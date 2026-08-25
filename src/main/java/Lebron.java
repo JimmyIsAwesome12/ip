71598fe4cc44e217f89c883c09766e8ff1896aea
JimmyIsAwesome12 <jimmyisawesome4@gmail.com>
2026-08-25 16:44:45 +0800

    Level-0

---
+++ src/main/java/Lebron.java
@@ -0,0 +1,19 @@
+public class Lebron {
+    private static final String LINE = "____________________________________________________________";
+    private static final String BANNER = " _     _____ ____  ____   ___  _   _ \n"
+            + "| |   | ____| __ )|  _ \\ / _ \\| \\ | |\n"
+            + "| |   |  _| |  _ \\| |_) | | | |  \\| |\n"
+            + "| |___| |___| |_) |  _ <| |_| | |\\  |\n"
+            + "|_____|_____|____/|_| \\_\\\\___/|_| \\_|\n";
+
+    public static void main(String[] args) {
+        System.out.println(LINE);
+        System.out.print(BANNER);
+        System.out.println("Hello! I'm Lebron.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
